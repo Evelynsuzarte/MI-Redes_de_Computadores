163e5bd07e9be223aed5e40d2c248abf42d50700
Evelynsuzarte <53132565+Evelynsuzarte@users.noreply.github.com>
2022-09-08 17:00:43 -0300

    atualização

--- PBL1/clienteServ.py
+++ PBL1/clienteServ.py
@@ -1,11 +1,30 @@
-
 import socket 
-ip = input('digite o ip de conexao: ') 
-port = 7000 
-addr = ((ip,port)) 
+""""
+ip = socket.gethostbyname(socket.gethostname())
+print(ip)
+port = 5050
+
+
+addr = (ip,port)
+
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM) 
 client_socket.connect(addr) 
+
 mensagem = input("digite uma mensagem para enviar ao servidor") 
 client_socket.send(mensagem) 
 print ('mensagem enviada' )
 client_socket.close()
+"""
+
+HOST = socket.gethostbyname(socket.gethostname())    # Endereco IP do Servidor
+PORT = 5000            # Porta que o Servidor esta
+tcp = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+dest = (HOST, PORT)
+tcp.connect(dest)
+
+print ('Para sair use CTRL+X\n')
+msg = input()
+while msg != '\x18':
+    tcp.send(bytes(msg, 'utf-8'))
+    msg = input()
+tcp.close()
